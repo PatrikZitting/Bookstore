6d0ffcf65f10bdf83e3e90cdf3d5a958aaf5035b
Patrik Zitting <patrik.zitting@hotmail.fi>
2023-02-13 04:20:06 +0200

    Adding booklist-page

--- src/main/java/hh/sof3as/Bookstore/web/BookController.java
+++ src/main/java/hh/sof3as/Bookstore/web/BookController.java
@@ -1,7 +1,11 @@
 package hh.sof3as.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hh.sof3as.Bookstore.domain.BookRepository;
 
 
 @Controller
@@ -10,5 +14,14 @@ public class BookController {
 	@RequestMapping("/index")
 	public String indexMessage() {
 		return "index";
+	}
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping("/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 }
