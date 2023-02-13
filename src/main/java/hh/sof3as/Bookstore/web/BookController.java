96013edcefc309999faee77bb57ff14c9078bb37
Patrik Zitting <patrik.zitting@hotmail.fi>
2023-02-13 05:02:39 +0200

    CRUD delete and add

--- src/main/java/hh/sof3as/Bookstore/web/BookController.java
+++ src/main/java/hh/sof3as/Bookstore/web/BookController.java
@@ -3,8 +3,10 @@ package hh.sof3as.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import hh.sof3as.Bookstore.domain.Book;
 import hh.sof3as.Bookstore.domain.BookRepository;
 
 
@@ -19,9 +21,27 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	
-	@RequestMapping("/booklist")
+	@RequestMapping(value="/booklist")
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+	
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addBook";
+    }
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveBook(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    } 
 }
