42cd8096d7cc1cdf19389b94b270719e7b85bf32
Patrik Zitting <patrik.zitting@hotmail.fi>
2023-02-27 00:09:10 +0200

    Categorylist and add functionality

---
+++ src/main/java/hh/sof3as/Bookstore/web/CategoryController.java
@@ -0,0 +1,36 @@
+package hh.sof3as.Bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import hh.sof3as.Bookstore.domain.Category;
+import hh.sof3as.Bookstore.domain.CategoryRepository;
+
+
+@Controller
+
+public class CategoryController {
+	@Autowired
+	private CategoryRepository repository;
+	
+	@RequestMapping(value="/categorylist")
+	public String categoryList(Model model) {
+		model.addAttribute("categories", repository.findAll());
+		return "categorylist";
+	}
+	
+	@RequestMapping(value = "/addcategory")
+    public String addCategory(Model model){
+    	model.addAttribute("category", new Category());
+        return "addCategory";
+    }
+	
+	@RequestMapping(value = "/savecategory", method = RequestMethod.POST)
+    public String saveCategory(Category category){
+        repository.save(category);
+        return "redirect:categorylist";
+    }
+}
